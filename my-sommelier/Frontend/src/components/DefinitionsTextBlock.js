3bb70e54a76b844a14bea192c518a6e37ad113e3
CharlesHuault <chuault@gmail.com>
2024-05-02 22:57:11 +0200

    Add some more definitions in Definitions page

--- my-sommelier/Frontend/src/components/DefinitionsTextBlock.js
+++ my-sommelier/Frontend/src/components/DefinitionsTextBlock.js
@@ -7,10 +7,43 @@ const DefinitionsTextBlock = () => {
             <h2>Définitions des filtres:</h2>
 
             <div>
+                <h3>Acide</h3>
+                <p>L'acidité, bien équilibrée, apporte fraîcheur et nervosité</p>
+                <h3>Ambré</h3>
+                <p>Vin jaune foncé</p>
+                <h3>Astringent</h3>
+                <p>Provoque une sensation de rugosité en bouche, liée aux tanins. Un rouge trop jeune est souvent astringent</p>
+                <h3>Biodynamique</h3>
+                <p>Type d'agriculture qui considère toute exploitation agricole comme un organisme vivant</p>
                 <h3>Boisé</h3>
-                <p>Se dit d’un vin dont l’odeur rappelle les fûts de chêne (fûts neufs) dans lesquels il a été élevé. Les amateurs parlent d’arôme de pain grillé, de torréfaction et de vanille.</p>
+                <p>Vin dont l’odeur rappelle les fûts de chêne (fûts neufs) dans lesquels il a été élevé. Les amateurs parlent d’arôme de pain grillé, de torréfaction et de vanille.</p>
+                <h3>Bouchonné</h3>
+                <p>Un vin bouchonné prend le goût de liège moisi, et donc désagréable à boire</p>
+                <h3>Brut</h3>
+                <p>Vin effervescent contenant peu de sucres</p>
+                <h3>Chaleureux</h3>
+                <p>Procure une sensation de chaleur en bouche, souvent due à une forte teneur en alcool</p>
+                <h3>Charnu</h3>
+                <p>Vin dense en bouche. Souvent riche en tanins et en alcool, il a du corps, de la consistance et de la matière. Un blanc moelleux est charnu, par exemple.</p>
+                <h3>Charpenté</h3>
+                <p>Vin équilibré, puissant, robuste et souvent tannique</p>
                 <h3>Herbacé</h3>
                 <p>Cet adjectif péjoratif décrit un vin qui dégage un parfum végétal, souvent dû à un manque de maturité du raisin.</p>
+                <h3>Long en bouche</h3>
+                <p>Vin dont l'arôme et les saveurs se maintiennent plusieurs secondes après dégustation</p>
+                <h3>Louche</h3>
+                <p>Vin dont la robe est légèrement trouble (aussi appelé vin trouble)</p>
+                <h3>Madérisé</h3>
+                <p>Vin qui s'est oxydé naturellement.</p>
+                <h3>Pâle</h3>
+                <p>Vin dont la robe est très claire, quasiment translucide</p>
+                <h3>Perlant</h3>
+                <p>Vin qui contient de fines bulles de gaz (ex: Gaillac perlé)</p>                
+                <h3>Plat</h3>
+                <p>Vin qui manque d'arômes ou d'acidité</p>
+                <h3>Végétal</h3>
+                <p>Qualificatif qui peut etre positif ou négatif. Arômes d'herbe fraichement coupée, de fougère, de tisane</p>
+
             </div>
         </div>
     );
